09788a39c45ca58cc8a3a4ea024e1ec92dcc2e8d
Andre Rodi <andre.rodi@gmail.com>
2026-03-26 23:55:26 +0100

    Add AndRod.Core project reference
    Implement ITypedCloneable<TSelf> on StronglyTypedId and switch
    IEquatable/IComparable to use TSelf instead of the record type. Update
    Equals and CompareTo signatures accordingly. Add explicit
    ITypedCloneable<TSelf>.Clone via StronglyTypedIdFactory.Create and a
    ToString override.

--- src/AndRod.StronglyTypedIds/StronglyTypedId.cs
+++ src/AndRod.StronglyTypedIds/StronglyTypedId.cs
@@ -1,9 +1,16 @@
-﻿namespace AndRod.StronglyTypedIds;
+﻿using AndRod.Core;
+
+namespace AndRod.StronglyTypedIds;
 
 /// <summary>
 /// A strongly-typed ID that wraps a value of type <typeparamref name="TValue"/>.
 /// </summary>
-public abstract record StronglyTypedId<TSelf, TValue>(TValue Value) : IStronglyTypedId<TValue>, IStronglyTypedId, IEquatable<StronglyTypedId<TSelf, TValue>>, IComparable<StronglyTypedId<TSelf, TValue>>
+public abstract record StronglyTypedId<TSelf, TValue>(TValue Value) :
+    IStronglyTypedId<TValue>,
+    IStronglyTypedId,
+    IEquatable<TSelf>,
+    IComparable<TSelf>,
+    ITypedCloneable<TSelf>
     where TSelf : StronglyTypedId<TSelf, TValue>
     where TValue : struct, IEquatable<TValue>, IComparable<TValue>
 {
@@ -23,18 +30,21 @@ public abstract record StronglyTypedId<TSelf, TValue>(TValue Value) : IStronglyT
     public bool IsTransient => Value.Equals(default);
 
     /// <inheritdoc />
-    public virtual bool Equals(StronglyTypedId<TSelf, TValue>? other)
+    public virtual bool Equals(TSelf? other)
     {
         if (other is null) return false;
         return Value.Equals(other.Value);
     }
 
     /// <inheritdoc />
-    public int CompareTo(StronglyTypedId<TSelf, TValue>? other)
+    public int CompareTo(TSelf? other)
     {
         if (other is null) return 1;
         return Value.CompareTo(other.Value);
     }
+
+    /// <inheritdoc />
+    TSelf ITypedCloneable<TSelf>.Clone() => StronglyTypedIdFactory.Create<TSelf, TValue>(Value);
 
     /// <summary>
     /// Returns the hash code of the underlying value.
@@ -50,4 +60,9 @@ public abstract record StronglyTypedId<TSelf, TValue>(TValue Value) : IStronglyT
     /// Returns an empty strongly-typed ID of type <see cref="TSelf"/> with the default value.
     /// </summary>
     public static TSelf Empty() => StronglyTypedIdFactory.Empty<TSelf, TValue>();
+
+    /// <summary>
+    /// Returns a string representation of the strongly-typed ID in the format "<see cref="TSelf"/>: <see cref="Value"/>".
+    /// </summary>
+    public override string ToString() => $"{typeof(TSelf).Name}: {Value}";
 }
